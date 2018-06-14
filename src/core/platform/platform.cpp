351bdf402dbc492029735492d23fd5622c460b10
David Tyler <dtyler69@gmail.com>
2018-06-14 17:38:54 -0400

    Initial work to add a build system and build on linux

--- src/core/platform/platform.cpp
+++ src/core/platform/platform.cpp
@@ -1,10 +1,16 @@
 #include "platform.h"
 
-//TODO: add in windows check here
+#ifdef WINDOWS
 #include "win_32/window/window_win32.h"
 #include "win_32/window/window_win32.cpp"
 #include "win_32/utils/utils_win32.h"
 #include "win_32/utils/utils_win32.cpp"
 #include "win_32/thread/thread_win32.h"
 #include "win_32/thread/thread_win32.cpp"
-
+#include "win_32/opengl/opengl.cpp"
+#include "win_32/opengl/shader.cpp"
+#include "win_32/opengl/vao.cpp"
+#include "win_32/opengl/vbo.cpp"
+#elif
+#error No files found for your platform
+#endif
--- src/core/platform/platform.h
+++ src/core/platform/platform.h
@@ -1,8 +1,16 @@
 #if !defined(PLATFORM_H)
 
+#ifdef WINDOWS
 #include "win_32/window/window_win32.h"
 #include "win_32/utils/utils_win32.h"
 #include "win_32/thread/thread_win32.h"
+#include "win_32/opengl/opengl.h"
+#include "win_32/opengl/shader.h"
+#include "win_32/opengl/vao.h"
+#include "win_32/opengl/vbo.h"
+#else
+#error No Headers for platform
+#endif
 
 #define PLATFORM_H
 #endif
