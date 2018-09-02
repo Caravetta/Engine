911d1173ea494974ef5b583aeeed5f8a0e3c9e70
Josh Caravetta <joshcaravetta@gmail.com>
2018-09-02 03:36:59 -0400

    Added in a new DEBUG_LOG macro along with a new CHECK/CHECK_INFO macro

--- src/core/core_common.h
+++ src/core/core_common.h
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <stdint.h>
+#include<typeinfo>
+//#include<cxxabi.h>
 #include "core_return.h"
 
 #define USE_BASE_MALLOC 0
@@ -18,9 +20,37 @@
 #define LOG_WARN(...) PRINT("WARN", __VA_ARGS__);
 #define LOG_ERROR(...) PRINT("ERROR", __VA_ARGS__);
 
+#ifdef DEBUG_BUILD
+#define DEBUG_LOG(...) PRINT("DEBUG", __VA_ARGS__);
+
+#define CHECK( expr ) CHECK_INFO( expr, "" )
+
+#define CHECK_INFO( expr, ... ){ \
+    if ( !(expr) ) { \
+        std::cout << __DATE__ << " - " << __TIME__ << " - " << "ERROR" << " - " << __FILENAME__ << " - " << __LINE__ << ": "; \
+        std::cout << "Assertion error [" << #expr << "] in " << __FUNCTION__ << "() at line " << __LINE__ << " in file " << __FILENAME__ << __VA_ARGS__ << std::endl; \
+    } \
+}
+#else
+#define DEBUG_LOG(...) while(false){}
+
+#define CHECK( expr ) while(false){}
+#endif
+
 #define PRINT(level,...){ \
     std::cout << __DATE__ << " - " << __TIME__ << " - " << level << " - " << __FILENAME__ << " - " << __LINE__; \
     std::cout << ": " << __VA_ARGS__ << std::endl; \
+}
+
+template<typename T>
+struct BaseType { typedef T type; };
+template<typename T>
+struct BaseType<T*> { typedef typename BaseType<T>::type type; };
+
+template<typename T>
+const char* realname (T &obj)
+{
+  return abi::__cxa_demangle(typeid(obj).name(), 0, 0, 0);
 }
 
 #define CORE_COMMON_H
